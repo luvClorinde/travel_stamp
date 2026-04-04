80a2ab19948cc88a3b9525cabe9a0204d7762c6f
luvchlorinde <nodachihiro1006@gmail.com>
2026-04-05 01:53:44 +0900

    add overseas/domestic change button

---
+++ src/pages/domestic/index.tsx
@@ -0,0 +1,10 @@
+function DomesticPage() {
+  return (
+    <div>
+      <h1>国内旅行</h1>
+      <p>国内旅行のコンテンツをここに追加してください。</p>
+    </div>
+  )
+}
+
+export default DomesticPage
---
+++ src/pages/overseas/index.tsx
@@ -0,0 +1,10 @@
+function OverseasPage() {
+  return (
+    <div>
+      <h1>海外旅行</h1>
+      <p>海外旅行のコンテンツをここに追加してください。</p>
+    </div>
+  )
+}
+
+export default OverseasPage
